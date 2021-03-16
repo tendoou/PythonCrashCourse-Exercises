6bce710e98a217ffeef4bc11a12d28a58c19d259
tendoou <zwei.stark@gmail.com>
2021-03-15 22:29:19 -0700

    Update Guess_number.py

--- Extras/Guess_number.py
+++ Extras/Guess_number.py
@@ -1,29 +1,29 @@
 import random
 
+picked_numbers = []
+
 
 def guessing_number():
     print('Think in a number from 10 to 100')
     print('Enter "q" to end the program')
     print(f'Enter y/n for yes or no ')
 
-    number_trials = 0
+    while len(picked_numbers) < 10:
+        guess_number = random.choice(range(1, 11, 1))
+        if guess_number not in picked_numbers:
+            picked_numbers.append(guess_number)
+            response = input(f'\nIs {guess_number} your number?:')
+            if response == 'q':
+                break
+            if response == 'y':
+                print('We won!')
+                break
+            else:
+                if len(picked_numbers) < 10:
+                    print('Ok, let me try again!')
 
-    while number_trials < 10:
-        guess_number = random.choice(range(1, 101, 1))
-
-        response = input(f'\nIs {guess_number} your number?:')
-        if response == 'q':
-            break
-        if response == 'y':
-            print('We won!')
-            break
-        else:
-            number_trials += 1
-            if number_trials < 9:
-                print('Ok, let me try again!')
-
-            elif number_trials == 10:
-                print('\nWelp, fuck you')
+                elif len(picked_numbers) == 10:
+                    print('\nWelp, fuck you')
 
 
 guessing_number()
